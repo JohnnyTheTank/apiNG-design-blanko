f96a655654ecccf4525f38831f8c585bed3c2c55
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-12-09 18:27:38 +0100

    impr: new apiKey mechanism

--- demo/js/aping-config.js
+++ demo/js/aping-config.js
@@ -2,37 +2,23 @@
 
 apingApp.config(['$provide', function ($provide) {
 
-    "use strict";
-    apingApp.config(['$provide', function ($provide) {
-
-        $provide.constant("apingApiKeys", {
-            youtube : [
-                {'apiKey':'<YOUR_YOUTUBE_API_KEY>'}
-            ],
-            instagram: [
-                {'access_token':'<YOUR_INSTAGRAM_TOKEN>'}
-            ],
-            facebook: [
-                {'access_token':'<YOUR_FACEBOOK_TOKEN>'}
-            ],
-            twitter: [
-                {'bearer_token':'<YOUR_TWITTER_BEARER_TOKEN>'}
-            ],
-            vimeo: [
-                {'access_token':'<YOUR_VIMEO_TOKEN>'}
-            ],
-        });
-
-        $provide.constant("apingDefaultSettings", {
-            templateUrl : "<PATH_TO_YOUR_DEFAULT_DESIGN>",
-            items : 20, //items per request
-            maxItems: 100, //max items per aping
-            orderBy : "timestamp",
-            orderReverse : "true",
-            model: "social",
-        });
-
-    }]);
+    $provide.constant("apingApiKeys", {
+        youtube : [
+            {'apiKey':'<YOUR_YOUTUBE_API_KEY>'}
+        ],
+        instagram: [
+            {'access_token':'<YOUR_INSTAGRAM_TOKEN>'}
+        ],
+        facebook: [
+            {'access_token':'<YOUR_FACEBOOK_TOKEN>'}
+        ],
+        twitter: [
+            {'bearer_token':'<YOUR_TWITTER_BEARER_TOKEN>'}
+        ],
+        vimeo: [
+            {'access_token':'<YOUR_VIMEO_TOKEN>'}
+        ],
+    });
 
     $provide.constant("apingDefaultSettings", {
         templateUrl : "../src/aping_design_sample.html",
