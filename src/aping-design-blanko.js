01bd5ae1f7dbfd16d5d8b11845496b4064ad6a21
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-04-25 16:44:28 +0200

    small improvments

--- src/aping-design-blanko.js
+++ src/aping-design-blanko.js
@@ -2,8 +2,7 @@
 
 angular.module('jtt_aping_design_blanko', [])
     .controller('apingBlankoDesignController', ['$scope', function ($scope) {
-
         $scope.$on('apiNG.resultMerged', function () {
-            $scope.workingCopy = $scope.results;
+            
         });
     }]);
